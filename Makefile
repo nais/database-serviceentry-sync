0cd6ef566f9f2e71074912c4008a7decb84b5ea8
Vegar Sechmann Molvig <vegar.sechmann.molvig@nav.no>
2020-02-12 14:24:53 +0100

    increase sleep duration to 30

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-image=docker.pkg.github.com/nais/database-serviceentry-sync/database-serviceentry-sync:0.5
+image=docker.pkg.github.com/nais/database-serviceentry-sync/database-serviceentry-sync:0.6
 
 build:
 	docker build -t ${image} .
--- main.sh
+++ main.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
-while sleep 1; do
+while sleep 30; do
   for ns in $(kubectl get namespace -o jsonpath="{ range .items[?(.metadata.annotations['cnrm\.cloud\.google\.com/project-id'])] }{.metadata.name } { end }"); do 
     ips=$(kubectl get sqlinstances.sql.cnrm.cloud.google.com --namespace "$ns" -o jsonpath="{ range .items[*].status.publicIpAddress }  {'-'} { @ }{ '/32\n' }")
     [[ -z "$ips"  ]] && echo "no databases in '$ns', skipping." && continue
